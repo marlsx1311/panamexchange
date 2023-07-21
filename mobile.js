958f7d7794d749f325a2329385f3531061cd31f3
marlonpccuarto <marlonaponte001@gmail.com>
2023-07-20 19:37:37 -0500

    se realiza la seccion de tasa del dia y se implemanta en js los cambios de variables de los valores de las tasas

--- mobile.js
+++ mobile.js
@@ -1,13 +1,24 @@
 const inbs = document.querySelector(".inBs");
 const btnCalcular = document.querySelector(".btn-calcular");
 const resulBs = document.querySelector(".resultado-bs");
-const resulUs = document.querySelector(".resultado-us")
+const resulUs = document.querySelector(".resultado-us");
+const act = document.querySelector(".tasa-fecha")
 
+window.addEventListener("load", actualizacion)
 
 btnCalcular.addEventListener("click" , calculoBs);
+let tasaDelDiaClp = 154;
 let tasaDelDiaCop = 154;
 let tasaDolar = 29.67;
+let fecha = " 40 de mayo"
 
+function actualizacion(){
+    moneda(tasaDelDiaCop, tasaDolar, tasaDelDiaClp, fecha)
+}
+
+function moneda(cop, us, clp, day){
+    act.innerText = day
+}
 function calculoBs(){
     valorUsuarioBs = inbs.value;
     let valorFinal = valorUsuarioBs / tasaDelDiaCop;
