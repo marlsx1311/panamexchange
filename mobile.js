d5bf72673fbcc5927118d06e5d1fa79898273a06
marlonpccuarto <marlonaponte001@gmail.com>
2023-07-22 21:05:01 -0500

    actualizando el slider

--- mobile.js
+++ mobile.js
@@ -6,18 +6,21 @@ const act = document.querySelector(".tasa-fecha");
 const spanUsd = document.querySelector(".spanusd");
 const spanCop = document.querySelector(".spancop");
 const spanClp = document.querySelector(".spanclp");
-
+const slider = document.querySelector(".carousel-slide")
+const imageSlider = slider.querySelectorAll("img")
 
 window.addEventListener("load", actualizacion)
-
 btnCalcular.addEventListener("click" , calculoBs);
+
+let index = 0
 let tasaDelDiaClp = 154;
 let tasaDelDiaCop = 154;
 let tasaDolar = 29.67;
-let fecha = " 40 de mayo"
+let fecha = " 21 de mayo"
 
 function actualizacion(){
     moneda(tasaDelDiaCop, tasaDolar, tasaDelDiaClp, fecha)
+    
 }
 
 function moneda(cop, us, clp, day){
@@ -34,3 +37,14 @@ function calculoBs(){
     let valorFinalUs = (valorUsuarioUs / tasaDelDiaCop)/tasaDolar ;
     resulUs.innerText = valorFinalUs.toFixed(2)+"USD";
 }
+
+setInterval(function(){
+    let sliderFun = index * -100;
+    slider.style.transform = "translateX("+ sliderFun +"%)";
+    index++
+    if(index > (imageSlider.length - 1)){
+        index = 0;
+    }
+    
+},5000);
+
